e6b50aba8902987e762aa1e53fbeeb5302a7f8b8
gabrielly koslovski <gaby@DESKTOP-RQQS146.>
2025-05-13 20:06:07 -0300

    first commit

---
+++ src/test/java/dev/books/BookRegistration/BookRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.books.BookRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
